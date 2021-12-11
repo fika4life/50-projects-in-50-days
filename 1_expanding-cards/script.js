166bfb91ccda5214251f503095c514777f55c09e
fika4life <fistewie@gmail.com>
2021-12-11 07:35:06 +0100

    adds expanding effect on click

---
+++ 1_expanding-cards/script.js
@@ -0,0 +1,16 @@
+//get panels
+let panels = document.querySelectorAll('.panel');
+
+//add event listeners to panels
+panels.forEach((panel) => {
+  panel.addEventListener('click', () => {
+    removeActiveClasses();
+    panel.classList.add('active');
+  });
+});
+
+function removeActiveClasses() {
+  panels.forEach((panel) => {
+    panel.classList.remove('active');
+  });
+}
